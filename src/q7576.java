021bf852b6629983273309a3b3926bb3140c3bbc
Van <vsh931025@gmail.com>
2019-02-13 11:08:10 +0900

    1. 토마토 해결

--- src/q7576.java
+++ src/q7576.java
@@ -1,10 +1,13 @@
-import java.awt.*;
 import java.util.*;
+
 /*
     문제 출처 : https://www.acmicpc.net/problem/7576
     이슈 사항
-    1.
+    1. 메모리 사용량이 너무 많음
+        -> 아마 xynum을 사용해서 인걸로 확인 됨
+        -> 포인터를 이용한 방식 생각해보기
  */
+
 
 public class q7576 {
     public int getnum(){
@@ -34,7 +37,6 @@ public class q7576 {
             }
         }
 
-        System.out.println(q);
 
         while(!q.isEmpty()){
             xynum temp = q.poll();
@@ -43,42 +45,27 @@ public class q7576 {
             int num = temp.num;
             if(q.isEmpty())
                 answer = num;
-            for(int i = 0; i < N; i++){
-                for(int j = 0; j < M; j++){
-                    System.out.print(tomato[i][j]+"     ");
-                }
-                System.out.println();
-            }
-            System.out.println("------------------------");
+
 
             if(x > 0 && tomato[x-1][y]==0){
                 tomato[x-1][y]=2;
                 q.offer(new xynum(x-1, y,num+1));
-                System.out.println(num+1);
             }
             if(x<N-1&&tomato[x+1][y]==0){
                 tomato[x+1][y]=2;
                 q.offer(new xynum(x+1,y,num+1));
-                System.out.println(num+1);
             }
             if(y>0&&tomato[x][y-1]==0){
                 tomato[x][y-1]=2;
                 q.offer(new xynum(x,y-1,num+1));
-                System.out.println(num+1);
             }
             if(y<M-1&&tomato[x][y+1]==0){
                 tomato[x][y+1]=2;
                 q.offer(new xynum(x,y+1,num+1));
-                System.out.println(num+1);
             }
-            for(int i = 0; i < N; i++){
-                for(int j = 0; j < M; j++){
-                    System.out.print(tomato[i][j]+"     ");
-                }
-                System.out.println();
-            }
-            System.out.println("answer = " + answer);
-            System.out.println();
+
+
+
         }
 
         for(int i = 0; i < N; i++){
--- src/xynum.java
+++ src/xynum.java
@@ -3,6 +3,7 @@
 */
 
 
+
 public class xynum {
     int x,y,num;
 
