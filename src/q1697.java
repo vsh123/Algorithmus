66efba524399656e62f633d184e7375712640ca4
Van <vsh931025@gmail.com>
2019-02-15 17:15:06 +0900

    1. q1697 숨바꼭질 해결

--- src/q1697.java
+++ src/q1697.java
@@ -1,13 +1,19 @@
 import java.util.*;
-import java.awt.*;
 import java.io.*;
 
 /*
     문제 : 숨바꼭질(BFS)
     문제 출처 : https://www.acmicpc.net/problem/1697
     이슈 사항
-    1.
+    1. 왜 x < 100000이여도 상관 없는지?
+        -> 100000으로 제한을 하니 visit[100001] 을 정의하고 해도 괜찮은데 이유를 모르겠음.
+    2. if((x-1) == K || (x+1) == K || (x*2) == K) {
+                answer = time + 1 ;
+                break;
+       }하면 틀렸다고 나옴
+       -> 이유를 모르겠음.........
  */
+
 public class q1697 {
     public int getpoint(){
         int answer = 0;
@@ -24,43 +30,38 @@ public class q1697 {
     }
 
     private int findtime(int N, int K){
-        int count = 1;
-        int ncount =1;
+
         int answer = 0;
-       Queue<Point> point = new LinkedList<Point>();
-       HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
+        ArrayList<Integer> point = new ArrayList<Integer>();
+        HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
 
-       point.offer(new Point(N,0));
-       map.put(N,0);
+        point.add(N);
+        map.put(N,0);
 
-       while(!point.isEmpty()){
-           Point temp = point.poll();
-           int x = temp.x;
-           int time = temp.y;
-           System.out.println("x = "+x+" time = "+ time);
-           if(x-1 == K || x+1 == K || x*2 == K) {
-               answer = time+1;
-               break;
-           }
-           ncount +=3;
-           if(!map.containsKey(x-1)&&x-1>0) {
-               point.offer(new Point(x - 1, time + 1));
-               map.put(x-1,0);
-               count++;
-           }
-           if(!map.containsKey(x+1)) {
-               point.offer(new Point(x + 1, time + 1));
-               map.put(x+1,0);
-               count++;
-           }
-           if(!map.containsKey(x*2)) {
-               point.offer(new Point(x * 2, time + 1));
-               map.put(x*2,0);
-               count++;
-           }
+        while(!point.isEmpty()){
+            int x = point.get(0);
+            int time = map.get(x);
+            point.remove(0);
+            if(x== K) {
+                answer = time;
+                break;
+            }
 
-       }
-       System.out.println("ncount = " + ncount + " count = " + count);
-       return answer;
+            if(!map.containsKey(x-1)&&(x-1)>=0) {
+                point.add(x-1);
+                map.put(x-1,time+1);
+            }
+            if(!map.containsKey(x+1)&&(x+1)<=100000) {
+                point.add(x+1);
+                map.put(x+1,time+1);
+            }
+            if(!map.containsKey(x*2)&&(x*2)<=100000) {
+                point.add(x*2);
+                map.put(x*2,time+1);
+            }
+
+        }
+        return answer;
     }
 }
+
