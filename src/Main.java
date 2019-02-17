3316f12961c134377acb00f69e9359c9a1ed7988
Sunho Van <vsh123@naver.com>
2019-02-18 00:00:17 +0900

    1. MazeMaker 작성

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        q11403 test = new q11403();
-        test.getgraph();
+        MazeMaker test = new MazeMaker();
+        test.getMaze();
     }
 }
