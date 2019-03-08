fc0a12e7bdfe9a95f58c4e61ba66f0c79a541c88
Sunho Van <vsh123@naver.com>
2019-03-08 17:41:31 +0900

    그리디~~~~~~~~힙

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        MakeBigNumber test = new MakeBigNumber();
-        test.solution(	"10000", 2);
+        IslandConnect test = new IslandConnect();
+        int[][] costs =
+                {{0,1,1},
+                        {0,2,2},
+                        {1,2,5},
+                        {1,3,1},
+                        {2,3,8}};
+        test.solution(	4, costs);
     }
 }
---
+++ src/RameonFactory.java
@@ -0,0 +1,19 @@
+import java.util.*;
+/*
+
+    문제 : 라면공장
+
+    출처 : https://programmers.co.kr/learn/courses/30/lessons/42629?language=java
+
+    이슈 사항
+        1.
+
+ */
+
+
+public class RameonFactory {
+    public int solution(int stock, int[] dates, int[] supplies, int k) {
+        int answer = 0;
+        return answer;
+    }
+}
