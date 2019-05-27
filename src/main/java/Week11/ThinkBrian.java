6edd327f446c95af6231a0ca220b631eeb2fef2e
SunHo Van <vsh123@naver.com>
2019-05-27 12:45:25 +0900

    refactor : gradle 프로젝트로 변경

---
+++ src/main/java/Week11/ThinkBrian.java
@@ -0,0 +1,18 @@
+package Week11;
+
+/*
+ * 소대소
+ * 대
+ * 대소대
+ * 대소대소
+ * */
+public class ThinkBrian {
+    public String solution(String sentence) {
+        String answer = "";
+        return answer;
+    }
+
+    private boolean isSmallLetter(char c) {
+        return 'a' <= c && c <= 'z';
+    }
+}
